b4f30bcb0dd229a5c4b930a478b7603a14a8c5db
JakubLeonik <leonikjakub@gmail.com>
2022-05-18 12:36:45 +0200

    add table and total pirce to shoppig card view

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,7 @@ class Order extends Model
 {
     protected $guarded = [];
     use HasFactory;
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
--- resources/views/dashboard/shopping-card.blade.php
+++ resources/views/dashboard/shopping-card.blade.php
@@ -3,9 +3,49 @@
         Green Shop
     </x-header>
     <x-center-pane>
-        @foreach ($orders as $order)
-            {{$order->product_id}} -> {{$order->user_id}} | {{$order->created_at}} <br>
-        @endforeach
+        <table class="table w-75 mx-auto">
+            <thead>
+            <tr>
+                <th scope="col">
+                    Product title
+                </th>
+                <th scope="col">
+                    Created At
+                </th>
+                <th scope="col">
+                    Price
+                </th>
+                <th scope="col">
+                    Status
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php $total_price = 0 ?>
+                @foreach ($orders as $order)
+                    <tr>
+                        <th scope="row">
+                            <a href="{{route('products.show', ['product' => $order->product->id])}}">
+                                {{$order->product->title}}
+                            </a>
+                        </th>
+                        <td>
+                            {{ date_format($order->created_at, 'j F Y') }}
+                        </td>
+                        <td>
+                            {{ $order->product->price }}$
+                            <?php $total_price += $order->product->price ?>
+                        </td>
+                        <td>
+                            {{ $order->status }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <span class="fw-bold">
+            Total price: {{ $total_price }}$
+        </span>
     </x-center-pane>
     {{ $orders->appends(request()->input())->links('paginators.b5') }}
     <div class="text-center w-100 pb-3">
